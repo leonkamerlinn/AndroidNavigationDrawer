bbb20a9873275209a84bfdbdadc927652f23e86f
Leon <leonkamerlinn@gmail.com>
2018-04-28 13:22:48 +0200

    init commit

---
+++ app/src/main/java/com/kamerlin/leon/drawerlayoutwithlistview/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.kamerlin.leon.drawerlayoutwithlistview
+
+import android.content.res.Configuration
+import android.os.Bundle
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+
+
+
+
+
+    val drawerLayout by lazy { findViewById<DrawerLayout>(R.id.drawer_layout) }
+    val drawerList by lazy { findViewById<ListView>(R.id.navList) }
+    val drawerToggle: ActionBarDrawerToggle by lazy {
+        object : ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_layout_open, R.string.drawer_layout_close) {
+
+            /** Called when a drawer has settled in a completely open state.  */
+            override fun onDrawerOpened(drawerView: View) {
+                super.onDrawerOpened(drawerView)
+                supportActionBar?.title = "Navigation!"
+                invalidateOptionsMenu() // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state.  */
+            override fun onDrawerClosed(view: View) {
+                super.onDrawerClosed(view)
+                supportActionBar?.title = getString(R.string.app_name)
+                invalidateOptionsMenu() // creates call to onPrepareOptionsMenu()
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+
+
+
+
+        val osArray = arrayOf("Android", "iOS", "Windows", "OS X", "Linux")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, osArray)
+        drawerList.adapter = adapter
+        drawerList.onItemClickListener = AdapterView.OnItemClickListener { _, _, _, _ -> Toast.makeText(this@MainActivity, "Time for an upgrade!", Toast.LENGTH_SHORT).show() }
+
+        drawerToggle.isDrawerIndicatorEnabled = true
+        drawerLayout.addDrawerListener(drawerToggle)
+
+
+    }
+
+
+
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        drawerToggle.onConfigurationChanged(newConfig)
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        // Activate the navigation drawer toggle
+        return if (drawerToggle.onOptionsItemSelected(item)) {
+            true
+        } else super.onOptionsItemSelected(item)
+
+    }
+}
